a66af1e7ba5ff1c8d595bfd338587f695bf3ea45
abhijeetkr21 <abhijeetkr21@gmail.com>
2017-04-22 15:34:48 +0530

    Formatting
    For better readability

--- HashMapDemo1.java
+++ HashMapDemo1.java
@@ -2,43 +2,44 @@ import java.util.*;
 
 class HashMapDemo1
 {
-public static void main(String args[])
-{
-HashMap<Emp,String>hm=new HashMap<Emp,String>();
-System.out.println(hm.size());
-hm.put(new Emp(10),"aaaa");
-hm.put(new Emp(10),"bbbb");
-hm.put(new Emp(10),"cccc");
-hm.put(new Emp(10),"dddd");
-Set s=hm.entrySet();
-Iterator i=s.iterator();
-while(i.hasNext())
-{
-Map.Entry e=(Map.Entry)i.next();
-Emp k=(Emp)e.getKey();
-String v=(String)e.getValue();
-System.out.println(k.id+"="+v);
+  public static void main(String args[])
+    {
+        HashMap<Emp,String>hm=new HashMap<Emp,String>();
+        System.out.println(hm.size());
+        hm.put(new Emp(10),"aaaa");
+        hm.put(new Emp(10),"bbbb");
+        hm.put(new Emp(10),"cccc");
+        hm.put(new Emp(10),"dddd");
+        Set s=hm.entrySet();
+        Iterator i=s.iterator();
+        while(i.hasNext())
+         {
+            Map.Entry e=(Map.Entry)i.next();
+            Emp k=(Emp)e.getKey();
+            String v=(String)e.getValue();
+            System.out.println(k.id+"="+v);
+         }
+     }
 }
-}
-}
+
 class Emp
 {
-int id;
-Emp(int id)
-{
-this.id=id;
-}
-public boolean equals(Object o)
-{
-Emp e=(Emp)o;
-if(e.id==this.id)
-return true;
-else
-return false;
-}
-public int hashCode()
-{
-return id*10;
-}
+    int id;
+    Emp(int id)
+     {
+       this.id=id;
+     }
+    public boolean equals(Object o)
+       {
+        Emp e=(Emp)o; 
+        if(e.id==this.id)
+        return true;
+        else
+        return false;
+       }
+    public int hashCode()
+      {
+        return id*10;
+      }
 }
 
